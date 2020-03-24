5dc780b3131aa5a1d4695f64425666b0c57d7765
Evgeny <melnikovevgeny960@gmail.com>
2020-03-24 16:08:52 +0300

    version 0.1.1
    add main window

--- app/src/main/java/com/example/schoolnews/MainActivity.java
+++ app/src/main/java/com/example/schoolnews/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.schoolnews;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView nav;
+    private FrameLayout frame;
+
+    private HomeFragment homeFragment;
+    private AddFragment addFragment;
+    private AccountFragment accountFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        frame = findViewById(R.id.frame);
+        nav = findViewById(R.id.nav);
+
+        homeFragment = new HomeFragment();
+        addFragment = new AddFragment();
+        accountFragment = new AccountFragment();
+
+        setFragment(homeFragment);
+
+        nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home :
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_add :
+                        setFragment(addFragment);
+                        return true ;
+                    case R.id.nav_account :
+                        setFragment(accountFragment);
+                        return true ;
+                    default :
+                        return false;
+                }
+            }
+        });
+
+        ActionBar actionBar =getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
